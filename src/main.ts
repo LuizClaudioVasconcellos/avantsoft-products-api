289e667e886f9a5e3bdb16378798d6f640751d1c
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2025-07-21 02:18:15 -0300

    feat(backend): configuração básica de cors para conexão com o front-end

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,18 @@ import { createDatabaseIfNotExists } from './database.provider';
 async function bootstrap() {
   try {
     await createDatabaseIfNotExists();
+
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+      origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type,Authorization',
+      credentials: true,
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    });
+
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
   } catch (error) {
@@ -13,4 +24,5 @@ async function bootstrap() {
     process.exit(1);
   }
 }
+
 bootstrap();
